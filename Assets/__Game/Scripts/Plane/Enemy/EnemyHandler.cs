01a81e2027bcf1431861cb0a2d7d8cacd7a4a0f5
dageron93 <vladb93games@gmail.com>
2024-03-10 22:46:52 +0200

    Added explosions

--- Assets/__Game/Scripts/Plane/Enemy/EnemyHandler.cs
+++ Assets/__Game/Scripts/Plane/Enemy/EnemyHandler.cs
@@ -1,3 +1,6 @@
+using Lean.Pool;
+using UnityEngine;
+
 namespace PlaneDestroyer
 {
   public class EnemyHandler : PlaneHandler
@@ -21,12 +24,33 @@ namespace PlaneDestroyer
     {
       Health -= damage;
 
-      if(Health <= 0)
+      if (Health <= DamagedThreshold && CurrentDamagedVFX == null)
+      {
+        SpawnDamageVFX();
+      }
+
+      if (Health <= 0)
       {
         Health = 0;
 
+        if (CurrentDamagedVFX != null)
+        {
+          LeanPool.Despawn(CurrentDamagedVFX);
+        }
+
+        SpawnDestroyVFX();
         Destroy(gameObject);
       }
+    }
+
+    protected override void SpawnDamageVFX()
+    {
+      CurrentDamagedVFX = LeanPool.Spawn(DamagedVFX, transform.position, Quaternion.identity, transform);
+    }
+
+    protected override void SpawnDestroyVFX()
+    {
+      LeanPool.Spawn(DeathVFX, transform.position, Quaternion.identity, null);
     }
   }
 }
--- Assets/__Game/Scripts/Plane/PlaneHandler.cs
+++ Assets/__Game/Scripts/Plane/PlaneHandler.cs
@@ -5,9 +5,20 @@ namespace PlaneDestroyer
   public abstract class PlaneHandler : MonoBehaviour
   {
     [SerializeField] protected int Health = 100;
+    [SerializeField] protected int DamagedThreshold = 25;
+
+    [Header("VFX")]
+    [SerializeField] protected GameObject DamagedVFX;
+    [SerializeField] protected GameObject DeathVFX;
+
+    protected GameObject CurrentDamagedVFX;
 
     protected PlaneDamageReceiver PlaneDamageReceiver;
 
     protected virtual void TakeDamage(int damage) { }
+
+    protected virtual void SpawnDamageVFX() { }
+
+    protected virtual void SpawnDestroyVFX() { }
   }
 }
